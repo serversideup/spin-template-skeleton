0d90555c9f1016c1b4e3c597b4cb1b1eb70c9678
Jay Rogers <jay@521dimensions.com>
2025-01-27 14:59:08 -0600

    Added install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+new() {
+  true
+}
+
+init() {
+  true
+}
+
+# When spin calls this script, it already sets a variable
+# called $SPIN_ACTION (that will have a value of "new" or "init")
+
+# Check to see if SPIN_ACTION function exists
+if type "$SPIN_ACTION" &>/dev/null; then
+  # Call the function
+  $SPIN_ACTION
+else
+  # If the function does not exist, throw an error
+  echo "The function '$SPIN_ACTION' does not exist."
+  exit 1
+fi
